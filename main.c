71f178c86892818dfd62e2c2e3222f8b147bf3d7
Asaf <asafkov@gmail.com>
2022-06-25 15:37:54 +0000

    - Dynamic linking fixed

--- main.c
+++ main.c
@@ -14,13 +14,12 @@ bool isElf(char *file_path);
 
 bool isExecutable(char *file_path);
 
-bool getFunctionInfo(char *file_path, char *func_name, bool *isGlobal, bool *is_dynamic, unsigned long* func_address, unsigned long* got_offset);
+bool getFunctionInfo(char *file_path, char *func_name, bool *isGlobal, bool *is_dynamic, unsigned long *func_address,
+                     unsigned long *got_offset);
 
 void debug(int pid, unsigned long func_address, bool is_dynamic, unsigned long got_offset);
 
-void print_run(int run_number, int return_value);
-
-static int counter = 0;
+void print_run(unsigned long run_number, int return_value);
 
 int main(int argc, char **argv) {
     if (argc < 2) {
@@ -50,51 +49,52 @@ int main(int argc, char **argv) {
     }
 
     int pid = fork();
-    if(pid == -1){
+    if (pid == -1) {
         // TODO: fork error
     }
-    if(pid == 0){
-        if(ptrace(PTRACE_TRACEME, 0, NULL, NULL) < 0){
+    if (pid == 0) {
+        if (ptrace(PTRACE_TRACEME, 0, NULL, NULL) < 0) {
             // TODO: ptrace error
         }
-        execv(file_path, argv+2);
+        execv(file_path, argv + 2);
     } else {
         debug(pid, func_address, is_dynamic, got_offset);
     }
 }
 
-void print_run(int run_number, int return_value){
-    printf("PRF:: run #%d returned with %d\n", run_number, return_value);
+void print_run(unsigned long run_number, int return_value) {
+    printf("PRF:: run #%lu returned with %d\n", run_number, return_value);
 }
 
-void debug(int pid, unsigned long func_address, bool is_dynamic, unsigned long got_offset){
+void debug(int pid, unsigned long func_address, bool is_dynamic, unsigned long got_offset) {
     unsigned long func_instruction, func_break_point, return_addr_instruction, return_break_point, plt_instruction, plt_breakpoint;
+    unsigned long entry_counter = 0, return_counter = 0;
     Elf64_Addr return_address, plt_address;
     int wait_status, value;
     struct user_regs_struct regs;
     wait(&wait_status);
 
-    while(is_dynamic && WIFSTOPPED(wait_status)){
+    while (is_dynamic && WIFSTOPPED(wait_status)) {
         // Put breaking point at PLT instruction
-	plt_address = ptrace(PTRACE_PEEKTEXT, pid, (void *) got_offset, NULL);
+        plt_address = ptrace(PTRACE_PEEKTEXT, pid, (void *) got_offset, NULL);
         plt_instruction = ptrace(PTRACE_PEEKTEXT, pid, (void *) plt_address, NULL);
         plt_breakpoint = (plt_instruction & 0xFFFFFFFFFFFFFF00) | 0xCC;
-        ptrace(PTRACE_POKETEXT, pid, (void*)plt_address, (void *) plt_breakpoint);
+        ptrace(PTRACE_POKETEXT, pid, (void *) plt_address, (void *) plt_breakpoint);
 
         // Wait for arriving at PLT breakpoint
         ptrace(PTRACE_CONT, pid, NULL, NULL);
         wait(&wait_status);
 
         // On arriving to PLT instruction
-        counter++;
+        entry_counter++;
         // Fix PLT instruction
-        ptrace(PTRACE_POKETEXT, pid, (void*)plt_address, (void*)plt_instruction);
+        ptrace(PTRACE_POKETEXT, pid, (void *) plt_address, (void *) plt_instruction);
         // Put breakpoint at rsp
         ptrace(PTRACE_GETREGS, pid, NULL, &regs);
-	return_address = ptrace(PTRACE_PEEKTEXT, pid, regs.rsp, NULL);
+        return_address = ptrace(PTRACE_PEEKTEXT, pid, regs.rsp, NULL);
         return_addr_instruction = ptrace(PTRACE_PEEKTEXT, pid, (void *) return_address, NULL);
         return_break_point = (return_addr_instruction & 0xFFFFFFFFFFFFFF00) | 0xCC;
-        ptrace(PTRACE_POKETEXT, pid, (void*)return_address, (void*)return_break_point);
+        ptrace(PTRACE_POKETEXT, pid, (void *) return_address, (void *) return_break_point);
         // Set rip at PLT instruction again
         regs.rip--;
         ptrace(PTRACE_SETREGS, pid, NULL, &regs);
@@ -102,15 +102,18 @@ void debug(int pid, unsigned long func_address, bool is_dynamic, unsigned long g
         // Wait for arrival at return-address breakpoint
         ptrace(PTRACE_CONT, pid, NULL, NULL);
         wait(&wait_status);
+        return_counter++;
         // Fix return address instruction
         ptrace(PTRACE_GETREGS, pid, NULL, &regs);
-        ptrace(PTRACE_POKETEXT, pid, (void*)return_address, (void*)return_addr_instruction);
+        ptrace(PTRACE_POKETEXT, pid, (void *) return_address, (void *) return_addr_instruction);
         // Set rip to redo return-address instruction
         regs.rip--;
         value = (int) regs.rax; // function return value
         ptrace(PTRACE_SETREGS, pid, NULL, &regs);
         // print result
-        print_run(counter, value);
+        if (entry_counter == return_counter) {
+            print_run(entry_counter, value);
+        }
         // GOT is now updated so can extract the function address
         func_address = ptrace(PTRACE_PEEKTEXT, pid, (void *) got_offset, NULL);
         is_dynamic = false;
@@ -118,38 +121,40 @@ void debug(int pid, unsigned long func_address, bool is_dynamic, unsigned long g
 
     func_instruction = ptrace(PTRACE_PEEKTEXT, pid, (void *) func_address, NULL);
     func_break_point = (func_instruction & 0xFFFFFFFFFFFFFF00) | 0xCC;
-    ptrace(PTRACE_POKETEXT, pid, (void*)func_address, (void*)func_break_point);
+    ptrace(PTRACE_POKETEXT, pid, (void *) func_address, (void *) func_break_point);
 
     ptrace(PTRACE_CONT, pid, NULL, NULL);
     wait(&wait_status);
 
-    while(WIFSTOPPED(wait_status)) {
-        counter++;
-
+    while (WIFSTOPPED(wait_status)) {
+        entry_counter++;
         // Fix the instruction at function entrance
-        ptrace(PTRACE_POKETEXT, pid, (void*)func_address, (void*)func_instruction);
+        ptrace(PTRACE_POKETEXT, pid, (void *) func_address, (void *) func_instruction);
         ptrace(PTRACE_GETREGS, pid, NULL, &regs);
         regs.rip--;
         // Put breakpoint at the return address
-	return_address = ptrace(PTRACE_PEEKTEXT, pid, regs.rsp, NULL);
+        return_address = ptrace(PTRACE_PEEKTEXT, pid, regs.rsp, NULL);
         return_addr_instruction = ptrace(PTRACE_PEEKTEXT, pid, (void *) return_address, NULL);
         return_break_point = (return_addr_instruction & 0xFFFFFFFFFFFFFF00) | 0xCC;
-        ptrace(PTRACE_POKETEXT, pid, (void*)return_address, (void*)return_break_point);
+        ptrace(PTRACE_POKETEXT, pid, (void *) return_address, (void *) return_break_point);
         ptrace(PTRACE_SETREGS, pid, NULL, &regs);
 
-	ptrace(PTRACE_CONT, pid, NULL, NULL);
+        ptrace(PTRACE_CONT, pid, NULL, NULL);
         wait(&wait_status);
         // Arrival at return address
+        return_counter++;
         // Fix return-address instruction
         ptrace(PTRACE_GETREGS, pid, NULL, &regs);
         value = (int) regs.rax;
-        ptrace(PTRACE_POKETEXT, pid, (void*)return_address, (void*)return_addr_instruction);
-        print_run(counter, value);
+        ptrace(PTRACE_POKETEXT, pid, (void *) return_address, (void *) return_addr_instruction);
+        if (entry_counter == return_counter) {
+            print_run(entry_counter, value);
+        }
         regs.rip--;
         ptrace(PTRACE_SETREGS, pid, NULL, &regs);
 
         // Put another breakpoint at function address
-        ptrace(PTRACE_POKETEXT, pid, (void*)func_address, (void*)func_break_point);
+        ptrace(PTRACE_POKETEXT, pid, (void *) func_address, (void *) func_break_point);
         ptrace(PTRACE_CONT, pid, NULL, NULL);
         wait(&wait_status);
     }
@@ -160,14 +165,14 @@ bool isElf(char *file_path) {
     char *initial_bytes = (char *) malloc(sizeof(char) * 5);
 
     int read_bytes = fread(initial_bytes, 1, 4, file);
-    if(read_bytes < 4){
+    if (read_bytes < 4) {
         free(initial_bytes);
         fclose(file);
         return false;
     }
     initial_bytes[4] = '\0';
 
-    bool is_elf = strcmp(initial_bytes+1, "ELF") == 0;
+    bool is_elf = strcmp(initial_bytes + 1, "ELF") == 0;
     free(initial_bytes);
     fclose(file);
     return is_elf;
@@ -185,12 +190,13 @@ bool isExecutable(char *file_path) {
     return elf_header->e_type == 2;
 }
 
-bool getFunctionInfo(char *file_path, char *func_name, bool *isGlobal, bool *is_dynamic, unsigned long *func_address, unsigned long *got_offset) {
+bool getFunctionInfo(char *file_path, char *func_name, bool *isGlobal, bool *is_dynamic, unsigned long *func_address,
+                     unsigned long *got_offset) {
     int fd;
     bool is_executable = false;
-    unsigned long symbol_entries = 0, realoc_entries = 0;
+    unsigned long symbol_entries = 0, rellaoc_entries = 0;
     char *string_table, *str_tab, *dyn_str, *curr_name, *section_name;
-    Elf64_Sym *dynamic_symbols, *realoc_table, *sym_tab;
+    Elf64_Sym *dynamic_symbols, *rellaoc_table, *sym_tab;
     Elf64_Shdr *section_header_arr, string_section;
     Elf64_Ehdr *elf_header;
     void *elf;
@@ -211,7 +217,7 @@ bool getFunctionInfo(char *file_path, char *func_name, bool *isGlobal, bool *is_
         section_name = section_header_arr[i].sh_name + string_table;
         if (section_header_arr[i].sh_type == 2 && strcmp(section_name, ".symtab") == 0) {
             symbol_entries = section_header_arr[i].sh_size /
-                              section_header_arr[i].sh_entsize; //size of section : size of each entry
+                             section_header_arr[i].sh_entsize; //size of section : size of each entry
             sym_tab = (Elf64_Sym *) ((char *) elf + section_header_arr[i].sh_offset);
         }
 
@@ -220,19 +226,21 @@ bool getFunctionInfo(char *file_path, char *func_name, bool *isGlobal, bool *is_
         }
 
         if (section_header_arr[i].sh_type == 4 && strcmp(section_name, ".rela.plt") == 0) {
-            realoc_entries = section_header_arr[i].sh_size /
-                             section_header_arr[i].sh_entsize; //size of section : size of each entry
-            realoc_table = (Elf64_Sym *) ((char *) elf + section_header_arr[i].sh_offset);
+            rellaoc_entries = section_header_arr[i].sh_size /
+                              section_header_arr[i].sh_entsize; //size of section : size of each entry
+            rellaoc_table = (Elf64_Sym *) ((char *) elf + section_header_arr[i].sh_offset);
         }
 
         if (section_header_arr[i].sh_type == 3 && strcmp(section_name, ".strtab") == 0) {
             if ((char *) elf + section_header_arr[i].sh_offset != string_table)
-                str_tab = ((char *) elf + section_header_arr[i].sh_offset); //getting the string table of the symbol table
+                str_tab = ((char *) elf +
+                           section_header_arr[i].sh_offset); //getting the string table of the symbol table
         }
 
         if (section_header_arr[i].sh_type == 3 && strcmp(section_name, ".dynstr") == 0) {
             if ((char *) elf + section_header_arr[i].sh_offset != string_table)
-                dyn_str = ((char *) elf + section_header_arr[i].sh_offset); //getting the string table of the symbol table
+                dyn_str = ((char *) elf +
+                           section_header_arr[i].sh_offset); //getting the string table of the symbol table
         }
     }
 
@@ -241,7 +249,7 @@ bool getFunctionInfo(char *file_path, char *func_name, bool *isGlobal, bool *is_
         curr_name = str_tab + sym_tab[i].st_name; //the current symbol name in the string table of symbol table
         if (!strcmp(curr_name, func_name)) {
             if (ELF64_ST_BIND(sym_tab[i].st_info) == 1) {
-                if(sym_tab[i].st_shndx == 0){ // st_shndx == 0 -> UND
+                if (sym_tab[i].st_shndx == 0) { // st_shndx == 0 -> UND
                     *is_dynamic = true;
                 }
                 *func_address = sym_tab[i].st_value;
@@ -252,14 +260,16 @@ bool getFunctionInfo(char *file_path, char *func_name, bool *isGlobal, bool *is_
     }
 
     // Search for the GOT offset of the function if needed
-    if(is_dynamic){
-        for(int i=0; i < realoc_entries; i++){
-            int entryIndex = ELF64_R_SYM(realoc_table[i].st_info);
+    Elf64_Rela *currentPlt = (Elf64_Rela *) (rellaoc_table);
+    if (is_dynamic) {
+        for (int i = 0; i < rellaoc_entries; i++) {
+            int entryIndex = ELF64_R_SYM(currentPlt->r_info);
             curr_name = dyn_str + dynamic_symbols[entryIndex].st_name;
-            if(strcmp(curr_name, func_name) == 0){
-                *got_offset = realoc_table[i].st_value; // Offset to GOT entry of func
+            if (strcmp(curr_name, func_name) == 0) {
+                *got_offset = currentPlt->r_offset; // Offset to GOT entry of func
                 break;
             }
+            currentPlt++;
         }
     }
 
